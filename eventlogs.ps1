1f40facd73631d3ab9738fa2b22913d380e3bd62
challa143143 <91412677+challa143143@users.noreply.github.com>
2022-04-04 11:13:22 +0000

    Add files via upload

---
+++ eventlogs.ps1
@@ -0,0 +1,5 @@
+﻿Get-EventLog -List
+Get-EventLog -LogName System -ComputerName dc1, AIQUM, wra, Win1 
+$Begin = Get-Date -Date '03/30/2019 10:00:00'
+$End = Get-Date -Date '03/31/2022 10:00:00'
+Get-EventLog -LogName System -EntryType Error -After $Begin -Before $End
